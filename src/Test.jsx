8e1ab1478ba3da130f6b93d7357e01bb850fa1c0
nolivercuny <dev@noliver.io>
2022-08-01 23:06:22 -0500

    working except random

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 const Test = ({data}) => {
-    const [state, setState] = useState({countries:data, selected: null});
+    const [state, setState] = useState({countries:data, selected: null, incorrect:[]});
 
     const handleClick = (e) => {
         const [type, value] = e.target.id.split(':');
@@ -10,14 +10,16 @@ const Test = ({data}) => {
             console.log(`value ${value} should match ${shouldMatch}`);
             if(shouldMatch === state.selected){
                 delete state.countries[type === 'country' ? value : Object.keys(state.countries).find(key => state.countries[key] === value)];
+                state.incorrect = [];
             }
             else{
-                console.log('incorrect')
-                //mark e.target as red
+                state.incorrect.push(state.selected);
+                state.incorrect.push(value);
             }
             setState({...state, selected: null});
         }else{
             console.log(`Setting selected to ${value}`)
+            state.incorrect = [];
             setState({...state, selected:value});
         }
     }
@@ -25,8 +27,8 @@ const Test = ({data}) => {
     return (
         <div>
         {state.countries && <div>
-        {Object.keys(state.countries).map((country) => <button style={{backgroundColor: state.selected === country ? '#000fff': ''}} id={`country:${country}`} onClick={handleClick}>{country}</button>)}
-        {Object.values(state.countries).map((capital) => <button id={`capital:${capital}`} onClick={handleClick}>{capital}</button>)}
+        {Object.keys(state.countries).map((country) => <button style={{backgroundColor: state.selected === country ? '#000fff': state.incorrect.includes(country) ? '#ff0000' : ''}} id={`country:${country}`} onClick={handleClick}>{country}</button>)}
+        {Object.values(state.countries).map((capital) => <button style={{backgroundColor: state.selected === capital ? '#000fff': state.incorrect.includes(capital) ? '#ff0000': ''}} id={`capital:${capital}`} onClick={handleClick}>{capital}</button>)}
         </div>}
         {!Object.keys(state.countries).length && <div>Congratulations!</div>}
         </div>
