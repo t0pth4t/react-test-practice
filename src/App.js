24641c5e88c18993a10be529a4fa0b4021e06d28
nolivercuny <dev@noliver.io>
2022-08-01 23:39:44 -0500

    add more countries

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Test from './Test';
 function App() {
   return (
     <div className="App">
-      <Test data={{"Germany":"Berlin", "United States": "Washington D.C."}}/>
+      <Test data={{"Germany":"Berlin", "United States": "Washington D.C.", "Afghanistan":"Kabul","Brunei":"Bandar Seri Begawan"}}/>
     </div>
   );
 }
